7a183ffc7c7206bc4007a1e690415ba131ff7d5e
Christian Wendt <christian.wendt@eduart-robotik.com>
2022-11-21 15:05:52 +0000

    Add ethernet gateway bot node.

--- src/node/ethernet_gateway_bot_node.cpp
+++ src/node/ethernet_gateway_bot_node.cpp
@@ -3,7 +3,7 @@
  *
  * Author: Christian Wendt (christian.wendt@eduart-robotik.com)
  */
-#include <edu_robot/ethernet_gateway/ethernet_gateway_shield.hpp>
+#include <edu_robot/ethernet_gateway/ethernet_gateway_bot.hpp>
 
 #include <rclcpp/executors.hpp>
 #include <rclcpp/rclcpp.hpp>
@@ -11,9 +11,7 @@
 int main(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
-  auto shield = std::make_shared<eduart::robot::ethernet::EthernetGatewayShield>("192.168.1.20", 2345);
-  // rclcpp::spin();
-  shield.reset();
+  rclcpp::spin(std::make_shared<eduart::robot::ethernet::EthernetGatewayBot>());
   rclcpp::shutdown();
 
   return 0;
