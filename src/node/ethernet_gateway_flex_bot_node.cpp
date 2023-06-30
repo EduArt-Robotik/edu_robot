977e86a806484dd2232043b335eb668e6b3ad31b
Christian Wendt <christian.wendt@eduart-robotik.com>
2023-06-30 12:26:17 +0000

    Move all bot definitions into node module.

--- src/node/ethernet_gateway_flex_bot_node.cpp
+++ src/node/ethernet_gateway_flex_bot_node.cpp
@@ -3,15 +3,59 @@
  *
  * Author: Christian Wendt (christian.wendt@eduart-robotik.com)
  */
-#include <edu_robot/ethernet_gateway/ethernet_gateway_flex_bot.hpp>
+#include <edu_robot/flex_bot/flex_bot.hpp>
+
+#include <edu_robot/ethernet_gateway/ethernet_gateway_shield.hpp>
+#include <edu_robot/ethernet_gateway/hardware_component_factory.hpp>
 
 #include <rclcpp/executors.hpp>
 #include <rclcpp/rclcpp.hpp>
 
+using eduart::robot::ethernet::EthernetGatewayShield;
+using eduart::robot::ethernet::HardwareComponentFactory;
+
+class EthernetGatewayFlexBot : public eduart::robot::flex_bot::FlexBot
+{
+public:
+  EthernetGatewayFlexBot()
+    : eduart::robot::flex_bot::FlexBot(
+        "flex_bot",
+        std::make_unique<EthernetGatewayShield>("192.168.1.20", 1234)
+      )
+  {
+    auto shield = std::dynamic_pointer_cast<EthernetGatewayShield>(_hardware_interface);
+    auto factory = HardwareComponentFactory(shield);
+    // _timer_process_status_report = create_wall_timer(100ms, [shield]{ shield->processStatusReport(); });
+
+           // Lightings
+    factory//.addLighting("head", "head_lighting")
+           //.addLighting("right_side", "right_side_lighting")
+           //.addLighting("left_side", "left_side_lighting")
+           //.addLighting("back", "back_lighting")
+           //.addLighting("all", "all_lighting")
+           // Motor Controller
+           .addMotorController("motor", "motor_hardware")
+           // Range Sensor
+           //.addRangeSensor("range/front/left", "range/front/left/hardware",
+           //                0u, RangeSensorHardware::Parameter{ })
+           //.addRangeSensor("range/front/right", "range/front/right/hardware",
+           //                1u, RangeSensorHardware::Parameter{ })
+           //.addRangeSensor("range/rear/left", "range/rear/left/hardware",
+           //                2u, RangeSensorHardware::Parameter{ })
+           //.addRangeSensor("range/rear/right", "range/rear/right/hardware",
+           //                3u, RangeSensorHardware::Parameter{ })
+           // IMU Sensor
+           .addImuSensor("imu", "imu_hardware", *this);
+
+    initialize(factory);
+    shield->registerComponentInput(_detect_charging_component);
+  }
+};
+
 int main(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<eduart::robot::ethernet::EthernetGatewayFlexBot>());
+  rclcpp::spin(std::make_shared<EthernetGatewayFlexBot>());
   rclcpp::shutdown();
 
   return 0;
