c278fa52bcba3b5f50bf4367e7d466b2ddee80df
Christian Wendt <christian.wendt@eduart-robotik.com>
2024-01-08 13:48:27 +0000

    Add flag for disabling IMU on IoT.

--- src/lib/iotbot_shield/imu_sensor_hardware.cpp
+++ src/lib/iotbot_shield/imu_sensor_hardware.cpp
@@ -23,13 +23,22 @@ void ImuSensorHardware::processRxData(const uart::message::RxMessageDataBuffer& 
     return;
   }
   
-  _callback_process_measurement(
-    uart::message::ShieldResponse::imuOrientation(data), Eigen::Vector3d::Zero(), Eigen::Vector3d::Zero()
-  );
+  if (_enable) {
+    _callback_process_measurement(
+      uart::message::ShieldResponse::imuOrientation(data), Eigen::Vector3d::Zero(), Eigen::Vector3d::Zero()
+    );
+  }
+  else {
+    _callback_process_measurement(
+      Eigen::Quaterniond(1.0, 0.0, 0.0, 0.0), Eigen::Vector3d::Zero(), Eigen::Vector3d::Zero()
+    );
+  }
 }
 
 void ImuSensorHardware::initialize(const SensorImu::Parameter& parameter)
 {
+  _enable = parameter.enable;
+
   // set IMU data mode
   auto request = ShieldRequest::make_request<uart::message::SetImuRawDataMode>(parameter.raw_data_mode, 0, 0, 0);
   auto response = _communicator->sendRequest(std::move(request));
