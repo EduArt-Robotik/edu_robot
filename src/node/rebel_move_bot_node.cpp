0e667ac71a2729e42247e3e003a48cbccef3986e
Christian Wendt <christian.wendt@eduart-robotik.com>
2024-05-21 13:44:18 +0000

    Adapt version for industrial edge. Add tof sensor ring to rebel move bot.

---
+++ src/node/rebel_move_bot_node.cpp
@@ -0,0 +1,68 @@
+/**
+ * Copyright EduArt Robotik GmbH 2023
+ *
+ * Author: Christian Wendt (christian.wendt@eduart-robotik.com)
+ */
+#include "edu_robot/hardware/can_gateway/sensor_tof_ring_hardware.hpp"
+#include "edu_robot/hardware/igus/motor_controller_hardware.hpp"
+#include <array>
+#include <edu_robot/bot/turtle/turtle.hpp>
+
+#include <edu_robot/hardware/igus/hardware_component_factory.hpp>
+#include <edu_robot/hardware/igus/can_gateway_shield.hpp>
+
+#include <rclcpp/executors.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+using eduart::robot::hardware::igus::CanGatewayShield;
+using eduart::robot::hardware::igus::HardwareComponentFactory;
+using eduart::robot::hardware::igus::MotorControllerHardware;
+using eduart::robot::hardware::can_gateway::SensorTofRingHardware;
+
+class IgusCanTurtleBot : public eduart::robot::turtle::Turtle
+{
+public:
+  IgusCanTurtleBot()
+    : eduart::robot::turtle::Turtle(
+        "turtle", std::make_unique<CanGatewayShield>(
+          "eduart-can2", "eduart-can1", "eduart-can0"
+        )
+      )
+  {
+    auto shield = std::dynamic_pointer_cast<CanGatewayShield>(_hardware_interface);
+    auto factory = HardwareComponentFactory(shield);
+
+    // Motor Controller
+    std::array<char const *const, 4> motor_controller_name = {
+      "motor_controller_a", "motor_controller_a", "motor_controller_a", "motor_controller_a"};
+
+    for (const auto& name : motor_controller_name) {
+      const auto parameter = MotorControllerHardware::get_parameter(
+        name, {}, *this);
+      factory.addMotorController(name, parameter);
+    }
+
+    // ToF Sensor Ring
+    auto point_cloud_parameter = SensorTofRingHardware::get_parameter(
+      "pointcloud_left",
+      {"pointcloud_left_a", "pointcloud_left_b"},
+      *this
+    );
+    factory.addTofRingSensor("pointcloud_left", point_cloud_parameter, *this);
+    
+    // IMU Sensor
+    factory.addImuSensor("imu", 0x381);           
+
+    initialize(factory);
+    _mode_state_machine.switchToMode(eduart::robot::RobotMode::INACTIVE);
+  }
+};
+
+int main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<IgusCanTurtleBot>());
+  rclcpp::shutdown();
+
+  return 0;
+}
