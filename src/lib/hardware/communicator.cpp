85c18be2e3f8c0db12c56a27f27fd4ba245762c5
Christian Wendt <christian.wendt@eduart-robotik.com>
2024-06-21 08:36:32 +0000

    Communicator accept empty search pattern now.

--- src/lib/hardware/communicator.cpp
+++ src/lib/hardware/communicator.cpp
@@ -82,7 +82,7 @@ message::RxMessageDataBuffer Communicator::getRxBuffer()
 template <typename Left, typename Right>
 static bool is_same(const Left& lhs, const Right& rhs) {
   if (lhs.empty()) {
-    // if left hand side is empty accept it --> response is matched to everything
+    // if search pattern is empty than accept message
     return true;
   }
   if (lhs.size() > rhs.size()) {
