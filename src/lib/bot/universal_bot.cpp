e1be5a246b1e348046fb8f74679015c31872d48d
Christian Wendt <christian.wendt@eduart-robotik.com>
2024-10-26 15:44:07 +0000

    3% Hirn

--- src/lib/bot/universal_bot.cpp
+++ src/lib/bot/universal_bot.cpp
@@ -86,28 +86,28 @@ void UniversalBot::initialize(eduart::robot::HardwareComponentFactory& factory)
   }
 
   // Point Cloud Sensors
-  constexpr std::array<const char*, 1> point_cloud_name = { "pointcloud_left" };
-  constexpr std::array<const char*, 1> point_cloud_tf = { "pointcloud/left" };
-  const std::array<tf2::Transform, 1> point_cloud_pose = {
-    tf2::Transform(tf2::Quaternion(0.0, 0.0, 0.0, 1.0), tf2::Vector3(0.0, 0.0, 0.0))
-  };
+  // constexpr std::array<const char*, 1> point_cloud_name = { "pointcloud_left" };
+  // constexpr std::array<const char*, 1> point_cloud_tf = { "pointcloud/left" };
+  // const std::array<tf2::Transform, 1> point_cloud_pose = {
+  //   tf2::Transform(tf2::Quaternion(0.0, 0.0, 0.0, 1.0), tf2::Vector3(0.0, 0.0, 0.0))
+  // };
   
-  for (std::size_t i = 0; i < point_cloud_name.size(); ++i) {
-    const auto parameter = robot::SensorPointCloud::get_parameter(
-      point_cloud_name[i], {}, *this);
-    auto hardware_interface = factory.hardware().at("pointcloud_left")->cast<robot::SensorPointCloud::SensorInterface>();
-    auto point_cloud_sensor = std::make_shared<robot::SensorPointCloud>(
-      point_cloud_name[i],
-      getFrameIdPrefix() + point_cloud_tf[i],
-      getFrameIdPrefix() + Robot::_parameter.tf.base_frame,
-      point_cloud_pose[i],
-      parameter,
-      *this,
-      hardware_interface
-    );
-    registerSensor(point_cloud_sensor);
-    hardware_interface->initialize(parameter);
-  }
+  // for (std::size_t i = 0; i < point_cloud_name.size(); ++i) {
+  //   const auto parameter = robot::SensorPointCloud::get_parameter(
+  //     point_cloud_name[i], {}, *this);
+  //   auto hardware_interface = factory.hardware().at("pointcloud_left")->cast<robot::SensorPointCloud::SensorInterface>();
+  //   auto point_cloud_sensor = std::make_shared<robot::SensorPointCloud>(
+  //     point_cloud_name[i],
+  //     getFrameIdPrefix() + point_cloud_tf[i],
+  //     getFrameIdPrefix() + Robot::_parameter.tf.base_frame,
+  //     point_cloud_pose[i],
+  //     parameter,
+  //     *this,
+  //     hardware_interface
+  //   );
+  //   registerSensor(point_cloud_sensor);
+  //   hardware_interface->initialize(parameter);
+  // }
 
   // IMU Sensor
   SensorImu::Parameter imu_parameter;
