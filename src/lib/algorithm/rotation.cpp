f8047c342433c845dc62834670574ca768cf8e6b
Christian Wendt <christian.wendt@eduart-robotik.com>
2025-04-04 08:23:22 +0000

    Implement alternative yaw angle calculation approach.

--- src/lib/algorithm/rotation.cpp
+++ src/lib/algorithm/rotation.cpp
@@ -6,12 +6,27 @@
 
 #include <Eigen/Geometry>
 
+#include <iostream>
+
 namespace eduart {
 namespace robot {
 namespace algorithm {
 
 AnglePiToPi quaternion_to_yaw(const geometry_msgs::msg::Quaternion& q) {
-  return std::atan2(2.0 * (q.z * q.w + q.x * q.y), -1.0 + 2.0 * (q.w * q.w + q.x * q.x));
+  // return std::atan2(2.0 * (q.z * q.w + q.x * q.y), -1.0 + 2.0 * (q.w * q.w + q.x * q.x));
+  const auto q_in = message::from_ros(q);
+  const Eigen::Vector3d b_x = Eigen::Vector3d::UnitX();
+
+  std::cout << "transformed b_x:" << b_x.transpose() << std::endl;
+  const Eigen::Vector3d b_x_transformed = q_in * b_x;
+  const Eigen::Vector2d b_x_transformed_2d = Eigen::Vector2d(b_x_transformed.x(), b_x_transformed.y()).normalized();
+
+  const double dot = Eigen::Vector2d::UnitX().dot(b_x_transformed_2d);
+  const double det = b_x_transformed_2d.y(); // det = x1 * y2 - y1 * x2 = 1 * y2 - 0 * x2 = y2
+  const AnglePiToPi yaw = std::atan2(det, dot);
+  std::cout << "yaw = " << yaw << std::endl;
+
+  return yaw;
 }
 
 void eliminate_yaw(geometry_msgs::msg::Quaternion &q)
