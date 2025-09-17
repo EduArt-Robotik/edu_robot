4022dbb1e882ff9c1a49f1ec80188461674b8615
Christian Wendt <christian.wendt@eduart-robotik.com>
2025-09-17 09:22:26 +0000

    Update package launch file for raspberry.

--- launch/eduard-raspberry-pi.launch.py
+++ launch/eduard-raspberry-pi.launch.py
@@ -13,6 +13,31 @@ from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+    # motor model
+    motor_model = os.environ.get('EDU_MOTOR_MODEL', 'faulhaber')    
+    # robot namespace
+    edu_robot_namespace = LaunchConfiguration('edu_robot_namespace')
+    edu_robot_namespace_arg = DeclareLaunchArgument(
+        'edu_robot_namespace', default_value=os.getenv('EDU_ROBOT_NAMESPACE', default='eduard')
+    )    
+
+    # pick corresponding parameter file
+    if motor_model == 'leison':
+      # Leison motor
+      motor_parameter_file = PathJoinSubstitution([
+         package_path,
+        'parameter',
+        'eduard_raspberry_motor_leison.yaml'
+      ])      
+    else:
+      # Faulhaber motor
+      motor_parameter_file = PathJoinSubstitution([
+        package_path,
+        'parameter',
+        'eduard_raspberry_motor_faulhaber.yaml'
+      ])
+
+    # Eduard Raspberry Pi Bot Node
     package_path = FindPackageShare('edu_robot')
     parameter_file = PathJoinSubstitution([
       package_path,
@@ -22,10 +47,13 @@ def generate_launch_description():
 
     eduard_ipc = Node(
       package='edu_robot',
-      executable='eduard-ethernet-gateway-bot',
+      executable='eduard-360-pi-bot',
       name='eduard',
-      parameters=[parameter_file],
-      namespace=EnvironmentVariable('EDU_ROBOT_NAMESPACE', default_value="eduard"),
+      parameters=[
+        parameter_file,
+        motor_parameter_file
+      ],
+      namespace=edu_robot_namespace,
       # prefix=['gdbserver localhost:3000'],
       output='screen'
     )
@@ -41,6 +69,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+      edu_robot_namespace_arg,
       eduard_ipc,
       aggregator
     ])
