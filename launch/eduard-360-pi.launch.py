fb6a49eb2b5bfb82fc4fe43f38558135e8d94102
Christian Wendt <christian.wendt@eduart-robotik.com>
2025-09-17 12:40:43 +0000

    rollback eduard raspberry launch file

--- launch/eduard-360-pi.launch.py
+++ launch/eduard-360-pi.launch.py
@@ -1,3 +1,5 @@
+import os
+
 from launch import LaunchDescription, LaunchContext
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
 from launch.conditions import IfCondition, UnlessCondition
@@ -27,6 +29,7 @@ def get_motor_parameter_file(context: LaunchContext, motor_model_arg: LaunchConf
         ]).perform(context)
 
 def generate_launch_description():
+    # get general parameters
     package_path = FindPackageShare('edu_robot')
     parameter_file = PathJoinSubstitution([
       package_path,
@@ -34,12 +37,20 @@ def generate_launch_description():
       'eduard-360-pi-bot.yaml'
     ])
 
+    # robot namespace
+    edu_robot_namespace = LaunchConfiguration('edu_robot_namespace')
+    edu_robot_namespace_arg = DeclareLaunchArgument(
+        'edu_robot_namespace', default_value=os.getenv('EDU_ROBOT_NAMESPACE', default='eduard')
+    )  
+
+    # get motor model
     motor_model_arg = DeclareLaunchArgument(
       'motor_model',
       default_value='faulhaber',
       description='Motor model to use (faulhaber or leison)'
     )
 
+    # Eduard 360 Pi Bot Node
     pi_bot = Node(
       package='edu_robot',
       executable='eduard-360-pi-bot',
@@ -48,7 +59,7 @@ def generate_launch_description():
         parameter_file,
         OpaqueFunction(function=get_motor_parameter_file, args=[LaunchConfiguration('motor_model')])
       ],
-      namespace=EnvironmentVariable('EDU_ROBOT_NAMESPACE', default_value="eduard"),      
+      namespace=edu_robot_namespace,      
       # prefix=['gdbserver localhost:3000'],
       output='screen'
     )
@@ -64,6 +75,7 @@ def generate_launch_description():
     )    
 
     return LaunchDescription([
+      edu_robot_namespace_arg,
       motor_model_arg,
       pi_bot,
       aggregator,
