f590bc669292ed9cd1d5fff4eb875f02d268ebda
Christian Wendt <christian.wendt@eduart-robotik.com>
2025-12-11 12:43:09 +0000

    Implement sub system test for can motor controller parameter handling.

--- src/test/sub_system/sub_system_test_can_motor_controller.cpp
+++ src/test/sub_system/sub_system_test_can_motor_controller.cpp
@@ -1,21 +1,84 @@
 #define CATCH_CONFIG_MAIN
 
-#include <catch2/catch_all.hpp>
+#include <catch2/catch_version_macros.hpp>
 
+#ifdef CATCH_VERSION_MAJOR
+#if CATCH_VERSION_MAJOR >= 3
+#include <catch2/catch_all.hpp>
+#else
+#include <catch2/catch.hpp>
+#endif
+#endif
+
+
+#include <edu_robot/hardware/can_gateway/can_communication_device.hpp>
 #include <edu_robot/hardware/can_gateway/motor_controller_hardware.hpp>
 
 // Tests the communication with can motor controller first. After some features are tested that can be tested via CAN interface.
 TEST_CASE("can communication", "[MotorControllerHardware]")
 {
   using namespace std::chrono_literals;
+  using eduart::robot::hardware::can_gateway::MotorControllerHardware;
+  using eduart::robot::hardware::can_gateway::CanCommunicationDevice;
+  using eduart::robot::hardware::Communicator;
+  using eduart::robot::Executer;
+  using eduart::robot::Motor;
 
-  const eduart::robot::hardware::can_gateway::MotorControllerHardware::Parameter parameter_hardware = {
+  const MotorControllerHardware::Parameter parameter_hardware = {
     {0x400, 0x480},
     16000,
     666ms,
     0.45f
   };
-  const eduart::robot::Motor::Parameter parameter_motor = {
-    
+  auto can_device = std::make_shared<CanCommunicationDevice>("eduart-can2", CanCommunicationDevice::CanType::CAN);
+  auto communicator = std::make_shared<Communicator>(can_device, 1ms);
+  auto executer = std::make_shared<Executer>(1ms);
+  MotorControllerHardware controller("can_motor_controller", parameter_hardware, executer, communicator);
+
+  const Motor::Parameter parameter_motor_a = {
+    true,
+    false,
+    0,
+    666.0,
+    47.11,
+    {
+      1.0,
+      2.0,
+      3.0,
+    },
+    {
+      false,
+      1313
+    }
+  };
+  const Motor::Parameter parameter_motor_b = {
+    false,
+    true,
+    0,
+    47.11,
+    666.0,
+    {
+      2.0,
+      3.0,
+      4.0,
+    },
+    {
+      false,
+      2323
+    }
+  };
+
+  SECTION("parameter_combination_one") {
+    // initial motors with 
+    std::vector<Motor::Parameter> parameter_motor_set = { parameter_motor_a, parameter_motor_b };
+
+    controller.initialize(parameter_motor_set);
+  }
+
+  SECTION("parameter_combination_two") {
+    // initial motors with 
+    std::vector<Motor::Parameter> parameter_motor_set = { parameter_motor_b, parameter_motor_a };
+
+    controller.initialize(parameter_motor_set);
   }
 }
