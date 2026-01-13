12ed5da9a3b4070e881e58c6323982ec780b21ee
Christian Wendt <christian.wendt@eduart-robotik.com>
2026-01-13 09:36:37 +0000

    Add zenoh configs.

---
+++ docker/iot2050/launch_content/zenoh_router.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# start the zenoh router for ROS 2 communication if wanted
+# the router allows communication between different networks
+
+if [[ ${RMW_IMPLEMENTATION} != "rmw_zenoh_cpp" ]]; then
+  # no need to launch a zenoh router
+  exit 0
+fi
+
+# launching zenoh router
+echo "Detected \"rmw_zenoh_cpp\" as RMW implementation was selected."
+echo "Starting zenoh router for ROS 2 communication..."
+
+export ZENOH_ROUTER_CONFIG_URI="./zenoh_router_config.json"
+ros2 run rmw_zenoh_cpp zenohd
