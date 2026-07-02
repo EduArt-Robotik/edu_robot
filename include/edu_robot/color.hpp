a698b55cd39dec82c9f59fe15dd81749ca3d7d17
Hannes Duske <152866008+hannesduske@users.noreply.github.com>
2026-07-02 15:53:32 +0000

    Implement brightness setting for lights

--- include/edu_robot/color.hpp
+++ include/edu_robot/color.hpp
@@ -15,6 +15,17 @@ struct Color
   static constexpr Color fromRgb(const std::uint8_t r, const std::uint8_t g, const std::uint8_t b) { return {r, g, b}; }
   // \todo add method fromHsv, usw...
 
+  Color operator*(const float &factor) const {
+    return {static_cast<std::uint8_t>(r * factor),
+            static_cast<std::uint8_t>(g * factor),
+            static_cast<std::uint8_t>(b * factor)};
+  }
+
+  Color &operator*=(const float &factor) {
+    *this = *this * factor;
+    return *this;
+  }
+
   std::uint8_t r;
   std::uint8_t g;
   std::uint8_t b;
